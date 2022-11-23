a63bda719263cf870c0d7396624f8968dd6fdaa6
Julia <yulia.bobko@mail.ru>
2022-11-23 16:12:08 +0300

    no message

--- src/api/api.js
+++ src/api/api.js
@@ -7,6 +7,7 @@ const instance = axios.create({
 
 });
 
+
 export const userAPI = {
     getUsers(currentPage, pageSize) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
